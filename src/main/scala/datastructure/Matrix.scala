19fe7d9e9ed17fc80a42946b82d0d5bbb65d4a1d
Prashant Sharma <prashant.s@imaginea.com>
2013-06-12 17:24:18 +0530

    Made matrix class immutable

--- src/main/scala/datastructure/Matrix.scala
+++ src/main/scala/datastructure/Matrix.scala
@@ -6,44 +6,37 @@ class MatrixIndexOutOfBoundsException(msg: String) extends RuntimeException(msg)
 
 /**
  * Matrix abstraction represents a matrix in row major order (intended!).
- * There are no constraints on m and n. If size of data is less than m x n
- * then it is zero padded to achieve the required size. And if size is more
- * than the size of data, then the extra data is simply ignored.
+ * There are no constraints on m and n except mxn should be data.length
  *
- * Adding above constraints can be easily added by extending Matrix and
- *  putting require inside it.
+ * This is supposed to be immutable.
+ *
+ * Adding more constraints should be easy.
  *
  */
-case class Matrix(name: String, m: Int, n: Int, var data: ArrayBuffer[Int]) {
+case class Matrix(name: String, m: Int, n: Int, private val data: ArrayBuffer[Int]) {
+
+  require(m * n == data.size, s"Can not initialize matrix with ${data.length} != $m * $n")
+
+  require(name.size != 0, "Name of the matrix can not be left empty!")
 
   override def toString(): String = {
     s"""\nMatrix $name dimensions $m x $n a row major order traversal\n""" + (for (w <- data.grouped(n)) yield (w.mkString(" "))).mkString("\n")
   }
 
-  def padTo(i: Int) = {
-    // helps in resizing
-    data = data.padTo(i, 0)
-  }
+  /** Not a user API. Used for testing.*/
+  private[datastructure] def getData = ArrayBuffer(data :_*)
 
   /** We think of it as padding if it does not exist in the array and yet in range of m x n */
   def rowMajorGet(i: Int, j: Int): Int = {
     if (i < m && j < n) {
-      if (i * n + j < data.size) data(i * n + j)
-      else 0
-    } else throw new MatrixIndexOutOfBoundsException(s"$name i:$i and j:$j out of bounds of $m x $n")
+      data(i * n + j)
+    } else 0
   }
 
-  /** We think of it as padding if it does not exist in the array and yet in range of m x n */
-  def rowMajorSet(i: Int, j: Int, item: Int): ArrayBuffer[Int] = {
+  /*This is not an api so that it remains immutable*/
+  private def rowMajorSet(i: Int, j: Int, item: Int) = {
     if (i < m && j < n) {
-      if (i * n + j < data.size) {
-        data(i * n + j) = item
-        data
-      } else { //Resize should not happen though.
-        padTo(i * n + j)
-        data(i * n + j) = item
-        data
-      }
+      data(i * n + j) = item
     } else throw new MatrixIndexOutOfBoundsException(s"$name i:$i and j:$j out of bounds of $m x $n")
   }
 
@@ -53,21 +46,20 @@ case class Matrix(name: String, m: Int, n: Int, var data: ArrayBuffer[Int]) {
    */
   def x(that: Matrix): Matrix = {
     require(n == that.m, s"Matrix multiplication not possible, A( $m x $n) X B(${that.m} x ${that.n})\n")
-    var c = Matrix("C", m, that.n, new ArrayBuffer())
-    c.padTo(m * that.n)
+    var c = Matrix("C", m, that.n, ArrayBuffer((0 until m * that.n).map(x => 0).toSeq: _*))
     for (i <- 0 until m; j <- 0 until that.n; k <- 0 until n) {
       c.rowMajorSet(i, j, c.rowMajorGet(i, j) + rowMajorGet(i, k) * that.rowMajorGet(k, j))
     }
     c
   }
 
-  def +=(that: Matrix): Matrix = {
+   def + (that: Matrix): Matrix = {
     require(n == that.n && m == that.m, s"Matrix addition not possible, Left( $m x $n) + Right(${that.m} x ${that.n})\n")
-
+    var c = Matrix("C", m, that.n, ArrayBuffer((0 until m * that.n).map(x => 0).toSeq: _*))
     for (i <- 0 until m; j <- 0 until that.n) {
-      rowMajorSet(i, j, rowMajorGet(i, j) + that.rowMajorGet(i, j))
+      c.rowMajorSet(i, j, rowMajorGet(i, j) + that.rowMajorGet(i, j))
     }
-    this
+    c
   }
 
 }
---
+++ src/test/scala/datastructure/MatrixSuite.scala
@@ -0,0 +1,78 @@
+package datastructure
+
+import org.scalatest.FunSuite
+import scala.collection.mutable.ArrayBuffer
+
+class MatrixSuite extends FunSuite {
+
+  val m = 5
+  val n = 4
+  val name = "A"
+  val data = ArrayBuffer(1 to (m * n): _*)
+  val mat = Matrix(name, m, n, data)
+
+  test("Requirement fails on initializing Matrix data with incorrect size ") {
+    val dataI = ArrayBuffer(1 to 100: _*)
+    val thrown = intercept[IllegalArgumentException] {
+      Matrix("BadMatrix", m, n, dataI)
+    }
+    assert(thrown.getMessage === s"requirement failed: Can not initialize matrix with ${dataI.length} != $m * $n")
+  }
+
+  test("Matrix can not be initialized with empty name ") {
+
+    val thrown = intercept[IllegalArgumentException] {
+      Matrix("", m, n, data)
+    }
+    assert(thrown.getMessage === s"requirement failed: Name of the matrix can not be left empty!")
+  }
+
+  test("Matrix instance is created with correct data") {
+    assert(mat.name === name)
+    assert(mat.m === m)
+    assert(mat.n === n)
+    assert(mat.getData === data)
+  }
+
+  test("rowMajorGet will return 0 if indices are not in range") {
+    val t1 = mat.rowMajorGet(m + 1, n)
+    val t2 = mat.rowMajorGet(m, n + 1)
+    assert(t1 === 0)
+    assert(t2 === 0)
+  }
+
+  test("Multiplying two matrices should return expected result"){
+    val matA = mat
+    val matB = Matrix("B", n, 2, ArrayBuffer( 1 to (n * 2) : _*) )
+    val expectedMat = Matrix("C", m, 2, ArrayBuffer(50, 60, 114, 140, 178, 220, 242, 300, 306, 380))
+    assert(expectedMat === (matA x matB))
+  }
+
+  test("Multiplying matrices of incompatible dimensions leads to exception"){
+    val matA = mat
+    val matB = Matrix("B", n-1, 2, ArrayBuffer( 1 to ((n-1) * 2) : _*) )
+    val thrown = intercept[IllegalArgumentException] {
+      matA x matB
+    }
+    assert(thrown.getMessage === s"requirement failed: Matrix multiplication not possible, A( $m x $n) X B(${matB.m} x ${matB.n})\n")
+  }
+
+
+  test("Adding matrices of incompatible dimensions leads to exception"){
+    val matA = mat
+    val matB = Matrix("B", n-1, 2, ArrayBuffer( 1 to ((n-1) * 2) : _*) )
+    val thrown = intercept[IllegalArgumentException] {
+      matA + matB
+    }
+    assert(thrown.getMessage === s"requirement failed: Matrix addition not possible, Left( $m x $n) + Right(${matB.m} x ${matB.n})\n")
+  }
+
+  test("Adding two matrices should return expected result"){
+    val matA = mat
+    val data2  = ArrayBuffer( (10 until (10 + (m*n))) : _*)
+    val matB = Matrix("B", m, n, data2)
+    val expectedMat = Matrix("C", m, n, ArrayBuffer(11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49))
+    assert(expectedMat === (matA + matB))
+  }
+
+}
