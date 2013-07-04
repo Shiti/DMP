8c91bdeaa0d63dfa91643e5ea66f4ed79f949c67
Prashant Sharma <scrapcodes@gmail.com>
2013-07-04 14:48:08 +0530

    Fixed batch file

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,6 +1,6 @@
 @echo off
 set AKKA_HOME=%~dp0..
 set AKKA_CLASSPATH=%AKKA_HOME%\config;%AKKA_HOME%\lib\*
-set JAVA_OPTS=-Xms256M -Xmx1024M
+set JAVA_OPTS=-Xms256M -Xmx1024M -Drm.pid=%2
 
-java %JAVA_OPTS% -cp "%AKKA_CLASSPATH%" -Dakka.home="%AKKA_HOME%" akka.kernel.Main %*
+java %JAVA_OPTS% -cp "%AKKA_CLASSPATH%" -Dakka.home="%AKKA_HOME%" akka.kernel.Main %1
